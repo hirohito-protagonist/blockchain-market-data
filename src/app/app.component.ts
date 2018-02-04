a870205e59abf1ee54d6aa7782a75fe60f54cc18
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2018-02-04 16:54:39 +0100

    chore: project setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bmd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+}
