bf57e9988bc050f30afab9dcc5d152a0fdf6426b
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2018-02-06 21:33:05 +0100

    feat: create core module
    In the core module the layout and entry
    application component will kept. Also it
    will agregate other features implemented
    in the future

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { CoreAppContainerComponent } from './containers/app.container';
+
+export {
+  CoreAppContainerComponent
+};
+
+@NgModule({
+  declarations: [ CoreAppContainerComponent ]
+})
+export class CoreModule {}
