aeab561ff33040dab74ee2970bba54b71caf9a07
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2018-03-06 21:51:07 +0100

    test: data service request effects

--- src/app/blockchain-data/effects/data-service.effect.spec.ts
+++ src/app/blockchain-data/effects/data-service.effect.spec.ts
@@ -64,4 +64,49 @@ describe('DataServiceEffects', () => {
 
     expect(effects.requestData$).toBeObservable(expected);
   });
+
+  it('should request convert to BTC on fetch data action and then dispatch response action', () => {
+
+    const responseData = 0.12331;
+    const action = new fromBlockchainDataAction.FetchData({
+      key: DataServiceType.ToBTC,
+      query: {
+        currency: 'USD',
+        value: 200
+      }
+    });
+    const completion = new fromDataServiceAction.Response({
+      key: DataServiceType.ToBTC,
+      response: responseData
+    });
+
+    actions$.stream = hot('-a', { a: action });
+    const response = cold('-a|', { a: responseData });
+    const expected = cold('--b', { b: completion });
+    spyOn(service, 'tobtc').and.returnValue(response);
+
+    expect(effects.responseToBTCData$).toBeObservable(expected);
+    expect(service.tobtc).toHaveBeenCalled();
+  });
+
+  it('should request market data prices on fetch data action and then dispatch response action', () => {
+
+    const responseData = {};
+    const action = new fromBlockchainDataAction.FetchData({
+      key: DataServiceType.Ticker,
+      query: null
+    });
+    const completion = new fromDataServiceAction.Response({
+      key: DataServiceType.Ticker,
+      response: responseData
+    });
+
+    actions$.stream = hot('-a', { a: action });
+    const response = cold('-a|', { a: responseData });
+    const expected = cold('--b', { b: completion });
+    spyOn(service, 'ticker').and.returnValue(response);
+
+    expect(effects.responseTcikerData$).toBeObservable(expected);
+    expect(service.ticker).toHaveBeenCalled();
+  });
 });
