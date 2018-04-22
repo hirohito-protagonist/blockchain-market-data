1fd7300d6b57c6de8e2463a57d6a7a918e3b9d0a
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2018-04-22 17:48:11 +0200

    test: add new

---
+++ src/app/statistics/reducers/ui.reducer.spec.ts
@@ -0,0 +1,44 @@
+import { fromUIActions } from './../actions/index.action';
+import { reducer, UIState } from './ui.reducer';
+import { reduce } from 'rxjs/operators';
+
+
+describe('UI reducer store state', () => {
+
+  describe('undefiend action', () => {
+
+    it('should return default state', () => {
+
+      const action = { type: void(0) } as fromUIActions.ActionType;
+      const result = reducer(void(0), action);
+
+      expect(result).toMatchSnapshot();
+    });
+  });
+
+  describe('[Statistics UI] update ui state', () => {
+
+    it('should not add new property to the store', () => {
+
+      const action = new fromUIActions.UpdateUIState({ key: 'test', value: 'test' });
+      const result = reducer(void(0), action);
+
+      expect(result).toMatchSnapshot();
+    });
+
+    it('should partial update state for provided key', () => {
+
+      const value1 = { selectedChart: 'test' };
+      let action = new fromUIActions.UpdateUIState({ key: 'chartsView', value: value1 });
+      let result = reducer(void(0), action);
+
+      expect(result).toMatchSnapshot();
+
+      const value2 = { selectedChartTimeSpan: '20days' };
+      action = new fromUIActions.UpdateUIState({ key: 'chartsView', value: value2 });
+      result = reducer(result, action);
+
+      expect(result).toMatchSnapshot();
+    });
+  });
+});
