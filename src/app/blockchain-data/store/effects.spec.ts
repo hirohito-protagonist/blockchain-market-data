0f51378ed5d8b3823a69d7a617e53e27a693175c
hirohito-protagonist <hirohito.protagonist@protonmail.com>
2020-05-16 20:14:51 +0200

    refactor: use TestScheduler instead jest-marbles

--- src/app/blockchain-data/store/effects.spec.ts
+++ src/app/blockchain-data/store/effects.spec.ts
@@ -2,8 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { Actions } from '@ngrx/effects';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { TestBed } from '@angular/core/testing';
-import { cold, hot } from 'jest-marbles';
-
+import { TestScheduler } from 'rxjs/testing';
 import { Observable, of } from 'rxjs';
 
 import { DataServiceEffects } from './effects';
@@ -11,6 +10,7 @@ import { ExchangeRatesService } from './../services/exchange-rates.service';
 import { StatisticsService } from './../services/statistics.service';
 import { fetchData, response, request } from './actions';
 import { DataServiceType } from '../blockchain-data.type';
+
 
 
 describe('DataServiceEffects', () => {
@@ -40,98 +40,128 @@ describe('DataServiceEffects', () => {
 
   it('should dispatch request data action on fetch action', () => {
 
-    // Given
-    const action = fetchData({
-      key: DataServiceType.Ticker, query: null
-    });
-    const completion = request({
-      key: DataServiceType.Ticker,
-      query: null
+    const testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
     });
 
-    // When
-    actions$ = hot('-a', { a: action });
-    const expected = cold('-b', { b: completion });
+    testScheduler.run(({ hot, cold, expectObservable }) => {
+      // Given
+      const action = fetchData({
+        key: DataServiceType.Ticker, query: null
+      });
+      const completion = request({
+        key: DataServiceType.Ticker,
+        query: null
+      });
 
-    // Then
-    expect(effects.requestData$).toBeObservable(expected);
+      // When
+      actions$ = hot('-a', { a: action });
+      const expected = cold('-b', { b: completion });
+
+      // Then
+      expectObservable(effects.requestData$).toBe('-b', {
+        b: completion
+      });
+    });
+
   });
 
   it('should request convert to BTC on fetch data action and then dispatch response action', () => {
 
-    // Given
-    const responseData = {
-      status: 200,
-      response: 0.12331
-    };
-    const action = fetchData({
-      key: DataServiceType.ToBTC,
-      query: {
-        currency: 'USD',
-        value: 200
-      }
-    });
-    const completion = response({
-      key: DataServiceType.ToBTC,
-      response: responseData.response
+    const testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
     });
 
-    // When
-    actions$ = hot('-a', { a: action });
-    const expected = cold('-b', { b: completion });
-    spyOn(exchangeRatesService, 'tobtc').and.returnValue(of(responseData));
+    testScheduler.run(({ hot, cold, expectObservable }) => {
+      // Given
+      const responseData = {
+        status: 200,
+        response: 0.12331
+      };
+      const action = fetchData({
+        key: DataServiceType.ToBTC,
+        query: {
+          currency: 'USD',
+          value: 200
+        }
+      });
+      const completion = response({
+        key: DataServiceType.ToBTC,
+        response: responseData.response
+      });
 
-    // Then
-    expect(effects.responseToBTCData$).toBeObservable(expected);
+      // When
+      actions$ = hot('-a', { a: action });
+      spyOn(exchangeRatesService, 'tobtc').and.returnValue(of(responseData));
+
+      // Then
+      expectObservable(effects.responseToBTCData$).toBe('-b', {
+        b: completion
+      });
+    });
   });
 
   it('should request market data prices on fetch data action and then dispatch response action', () => {
 
-    // Given
-    const responseData = {
-      status: 200,
-      response: {}
-    };
-    const action = fetchData({
-      key: DataServiceType.Ticker,
-      query: null
-    });
-    const completion = response({
-      key: DataServiceType.Ticker,
-      response: responseData.response
+    const testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
     });
 
-    // When
-    actions$ = hot('-a', { a: action });
-    const expected = cold('-b', { b: completion });
-    spyOn(exchangeRatesService, 'ticker').and.returnValue(of(responseData));
+    testScheduler.run(({ hot, cold, expectObservable }) => {
+      // Given
+      const responseData = {
+        status: 200,
+        response: {}
+      };
+      const action = fetchData({
+        key: DataServiceType.Ticker,
+        query: null
+      });
+      const completion = response({
+        key: DataServiceType.Ticker,
+        response: responseData.response
+      });
 
-    // Then
-    expect(effects.responseTcikerData$).toBeObservable(expected);
+      // When
+      actions$ = hot('-a', { a: action });
+      spyOn(exchangeRatesService, 'ticker').and.returnValue(of(responseData));
+
+      // Then
+      expectObservable(effects.responseTcikerData$).toBe('-b', {
+        b: completion
+      });
+    });
   });
 
   it('should request stats information on fetch data action and then dispatch response action', () => {
 
-    // Given
-    const responseData = {
-      status: 200,
-      response: {}
-    };
-    const action = fetchData({
-      key: DataServiceType.Stats,
-      query: null
-    });
-    const completion = response({
-      key: DataServiceType.Stats,
-      response: responseData.response
+    const testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
     });
 
-    // When
-    actions$ = hot('-a', { a: action });
-    const expected = cold('-b', { b: completion });
-    spyOn(statisticsService, 'stats').and.returnValue(of(responseData));
+    testScheduler.run(({ hot, cold, expectObservable }) => {
+      // Given
+      const responseData = {
+        status: 200,
+        response: {}
+      };
+      const action = fetchData({
+        key: DataServiceType.Stats,
+        query: null
+      });
+      const completion = response({
+        key: DataServiceType.Stats,
+        response: responseData.response
+      });
 
-    // Then
-    expect(effects.responseStatsData$).toBeObservable(expected);
+      // When
+      actions$ = hot('-a', { a: action });
+      spyOn(statisticsService, 'stats').and.returnValue(of(responseData));
+
+      // Then
+      expectObservable(effects.responseStatsData$).toBe('-b', {
+        b: completion
+      });
+    });
   });
 });
