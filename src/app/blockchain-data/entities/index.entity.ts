f24f9eabf13e34adeed845310f44337e5455e4f1
HiroHito Protagonist <hirohito.protagonist@protonmail.com>
2023-10-24 21:39:09 +0200

    chore: remove files

--- src/app/blockchain-data/entities/index.entity.ts
+++
@@ -1,7 +0,0 @@
-import * as fromBlockchainDataMarketPriceEntity from './market-price.entity';
-import * as fromBlockchainDataStatisticsEntity from './statistic.entity';
-
-export {
-  fromBlockchainDataMarketPriceEntity,
-  fromBlockchainDataStatisticsEntity
-};
--- src/app/core/store/actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { LayoutView } from './layout.reducer';
-
-
-export const changeView = createAction(
-  '@core/layout-view',
-  props<{ view: LayoutView }>()
-);
